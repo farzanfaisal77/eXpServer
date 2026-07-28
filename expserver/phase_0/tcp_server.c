16f71e1440705ed59f2be6e3a2e154c30c51fa6e
farzanfaisal77 <farzanfaisal77@gmail.com>
2026-07-28 11:55:55 +0530

    Stage 1 Miletsone 1

--- expserver/phase_0/tcp_server.c
+++ expserver/phase_0/tcp_server.c
@@ -22,6 +22,33 @@ int main(){
     // SOCK_STREAM - Stream socket, TCP
     // 0 - default protocol
 
+    //to bind the socket to same IP and Port while restarting
     int enable = 1;
     setsockopt (listen_sock_fd,SOL_SOCKET,SO_REUSEADDR, &enable, sizeof(int));
+
+    struct sockaddr_in server_addr;
+
+    //setting up server address
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY); //assings IP Address. INADDR_ANY represents any available IP on host device
+    //htonl() host byte order to network byte order
+    server_addr.sin_port = htons(PORT); // assigning port
+    //htons same function as htonl
+
+    //now we need to bind the listening socket to specified port and ip
+    bind(listen_sock_fd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+
+    //now we need to listen to the port
+    listen(listen_sock_fd, MAX_ACCEPPT_BACKLOG);
+    printf("!!! SERVER LISTENING TO PORT %d\n", PORT);
+
+    //client connection and client connection details
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_len;
+
+    //accepting connection
+    int conn_sock_fd = accept (listen_sock_fd, (struct sockaddr*)&client_addr, &client_addr_len);
+    //prgm will block here until a client has connected
+    printf("WOW BRO CLIENT CONNECTED!!!\n");
+    
 }
