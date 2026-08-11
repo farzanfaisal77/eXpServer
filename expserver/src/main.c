4147556756d7c560beee143b71610df2abc33a14
farzanfaisal77 <farzanfaisal77@gmail.com>
2026-08-11 11:32:09 +0530

    xps_connection and listener (revison needed)

---
+++ expserver/src/main.c
@@ -0,0 +1,16 @@
+/* main()
+  create event loop
+  create listeners, each listening on different ports
+  run event loop
+
+loop_create()
+  create event loop instance and return it
+
+loop_attach()
+  attach FD to event loop
+
+loop_detach()
+  detach FD from event loop
+
+loop_run()
+  run event loop */
