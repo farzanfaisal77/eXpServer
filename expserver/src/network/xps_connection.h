821cf870abec8ac272bd41967dfe55351f5b384f
farzanfaisal77 <farzanfaisal77@gmail.com>
2026-08-11 17:52:17 +0530

    connection module

--- expserver/src/network/xps_connection.h
+++ expserver/src/network/xps_connection.h
@@ -7,9 +7,11 @@ struct xps_connection_s {
     int epoll_fd;
     int sock_fd;
     xps_listener_t *listener;
+    char *remote_ip;
 };
 
 xps_connection_t *xps_connection_create(int epoll_fd, int sock_fd);
 void xps_connection_destroy(xps_connection_t *connection);
+void xps_connection_read_handler(xps_connection_t *connection);
 
 #endif
