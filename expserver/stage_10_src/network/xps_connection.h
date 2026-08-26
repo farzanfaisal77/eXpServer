325b27c8fd40aeeaf3eeb998767400480d8e6859
farzanfaisal77 <farzanfaisal77@gmail.com>
2026-08-26 15:11:45 +0530

    stage_10 start (will complete in 1 week probably)

---
+++ expserver/stage_10_src/network/xps_connection.h
@@ -0,0 +1,28 @@
+#ifndef XPS_CONNECTION_H
+#define XPS_CONNECTION_H
+
+#include "../xps.h"
+
+struct xps_connection_s {
+    xps_core_t *core;
+    int sock_fd;
+    xps_listener_t *listener;
+    char *remote_ip;
+    xps_buffer_list_t *write_buff_list;
+    bool read_ready;
+    bool write_ready;
+    xps_handler_t send_handler;
+    xps_handler_t recv_handler;
+};
+
+xps_connection_t *xps_connection_create(xps_core_t *core, u_int sock_fd);
+void xps_connection_destroy(xps_connection_t *connection);
+void connection_read_handler(void *ptr);
+void connection_write_handler(void* ptr);
+void connection_close_handler(void* ptr);
+void connection_loop_read_handler(void *ptr);
+void connection_loop_write_handler(void* ptr);
+
+void strrev(char *str);
+
+#endif
