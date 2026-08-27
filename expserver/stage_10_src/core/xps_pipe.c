8638b6ae99892cf7ee9f75d75b169fb986644278
farzanfaisal77 <farzanfaisal77@gmail.com>
2026-08-27 18:20:40 +0530

    sorce, sink attatch/dettatch

--- expserver/stage_10_src/core/xps_pipe.c
+++ expserver/stage_10_src/core/xps_pipe.c
@@ -28,8 +28,10 @@ xps_pipe_t *xps_pipe_create(xps_core_t *core, size_t buff_thresh, xps_pipe_sourc
     vec_push(&core->pipes, pipe);
 
     /*Attach source and sink to pipe*/
-    pipe->source = source;
-    pipe->sink = sink;
+    /* pipe->source = source;
+    pipe->sink = sink; */
+    xps_pipe_attach_source(pipe, source);
+    xps_pipe_attach_sink(pipe, sink);
     /*Make both source and sink of pipe active*/
 
     logger(LOG_DEBUG, "xps_pipe_create()", "created pipe");
@@ -54,3 +56,68 @@ void xps_pipe_destroy(xps_pipe_t *pipe) {
     free(pipe);
     logger(LOG_DEBUG, "xps_pipe_destroy()", "destroyed pipe");
 }
+
+bool xps_pipe_is_readable(xps_pipe_t *pipe){ 
+    return pipe->buff_list->len > 0;
+}
+
+bool xps_pipe_is_writable(xps_pipe_t *pipe){
+    return pipe->buff_list->len < DEFAULT_PIPE_BUFF_THRESH;
+}
+
+int xps_pipe_attach_source(xps_pipe_t *pipe, xps_pipe_source_t *source) {
+    /*assert pipe and source not null*/
+    assert(pipe!=NULL);
+    assert(source!=NULL);
+    /*check whether pipe already has a source and return E_FAIL*/
+
+    if(pipe->source != NULL) return E_FAIL;
+
+    pipe->source = source;
+    source->pipe = pipe;
+
+    return OK;
+}
+
+int xps_pipe_detach_source(xps_pipe_t *pipe) {
+    /*assert pipe not null*/
+    assert(pipe!=NULL);
+
+    /*check whether pipe has no source and return E_FAIL*/
+    if(pipe->source == NULL) return E_FAIL;
+
+    pipe->source->pipe = NULL;
+    pipe->source = NULL;
+
+    return OK;
+}
+
+
+int xps_pipe_attach_sink(xps_pipe_t *pipe, xps_pipe_sink_t *sink) {
+    /*assert pipe and sink not null*/
+    assert(pipe!=NULL);
+    assert(sink!=NULL);
+
+    /*check whether pipe already has a sink and return E_FAIL*/
+    if(pipe->sink != NULL) return E_FAIL;
+
+    pipe->sink = sink;
+    /* pipe->sink->pipe = pipe; */
+    sink->pipe = pipe;
+
+    return OK;
+}
+
+int xps_pipe_detach_sink(xps_pipe_t *pipe) {
+    /*assert pipe not null*/
+    assert(pipe!=NULL);
+
+    /*check whether pipe has no sink and return E_FAIL*/
+    if(pipe->sink == NULL) return E_FAIL;
+
+    pipe->sink->pipe = NULL;
+    pipe->sink = NULL;
+
+    return OK;
+}
+
