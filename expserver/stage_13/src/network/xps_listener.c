eb6f98d9a9b8daa5e823d94d9ad84010765feb03
farzanfaisal77 <farzanfaisal77@gmail.com>
2026-09-24 17:29:00 +0530

    listener.c done

--- expserver/stage_13/src/network/xps_listener.c
+++ expserver/stage_13/src/network/xps_listener.c
@@ -106,70 +106,41 @@ void listener_connection_handler(void* ptr) {
   xps_listener_t* listener = ptr;
 
   while(1){
-    // Accepting connection
-    struct sockaddr conn_addr;
-    socklen_t conn_addr_len = sizeof(conn_addr);
-    int conn_sock_fd = accept(listener->sock_fd, (struct sockaddr*)&conn_addr, &conn_addr_len); 
-    if (conn_sock_fd < 0) {
-      if((errno == EAGAIN || errno == EWOULDBLOCK)){
-        break;
-      }
-    logger(LOG_ERROR, "xps_listener_connection_handler()", "accept() failed");
-    perror("Error message");
-    return;
-    }
+	// Accepting connection
+	struct sockaddr conn_addr;
+	socklen_t conn_addr_len = sizeof(conn_addr);
+	int conn_sock_fd = accept(listener->sock_fd, (struct sockaddr*)&conn_addr, &conn_addr_len); 
+	if (conn_sock_fd < 0) {
+		if((errno == EAGAIN || errno == EWOULDBLOCK)){
+			break;
+		}
+		logger(LOG_ERROR, "xps_listener_connection_handler()", "accept() failed");
+		perror("Error message");
+		return;
+	}
 
-    if(make_socket_non_blocking(conn_sock_fd) != OK){
-    logger(LOG_ERROR, "xps_listener_connection_handler()", "not able to make non blocking");
-    perror("Error message");
-    close(conn_sock_fd);
-    return;
-    }
+	if(make_socket_non_blocking(conn_sock_fd) != OK){
+		logger(LOG_ERROR, "xps_listener_connection_handler()", "not able to make non blocking");
+		perror("Error message");
+		close(conn_sock_fd);
+		return;
+	}
 
-    // Creating connection instance
-    xps_connection_t *client = xps_connection_create(listener->core, conn_sock_fd);
-    if (client == NULL) {
-    logger(LOG_ERROR, "xps_listener_connection_handler()", "xps_connection_create() failed");
-    close(conn_sock_fd);
-    return;
-    }
-    
-    if(listener->port == 8001){
-      logger(LOG_INFO, "xps_listener_connection_handler()", "Creating upstream connection to 127.0.0.1:3000");
-      xps_connection_t* connection = xps_upstream_create(listener->core, "127.0.0.1", 3000);
-      if (connection == NULL) {
-        logger(LOG_ERROR, "xps_listener_connection_handler()", "xps_upstream_create() failed");
-        xps_connection_destroy(client);
-        return;
-      }
-      xps_pipe_create(listener->core,DEFAULT_PIPE_BUFF_THRESH,client->source, connection->sink);
-      xps_pipe_create(listener->core,DEFAULT_PIPE_BUFF_THRESH,connection->source, client->sink);
-    }
+	// Creating connection instance
+	xps_connection_t *client = xps_connection_create(listener->core, conn_sock_fd);
+		if (client == NULL) {
+		logger(LOG_ERROR, "xps_listener_connection_handler()", "xps_connection_create() failed");
+		close(conn_sock_fd);
+		return;
+	}
+	client->listener = listener;
 
-    else if (listener->port == 8002) {
-      int error;
-      xps_file_t *file = xps_file_create(listener->core, "../public/sample.txt", &error);
-      if(!file) {
-          xps_connection_destroy(client);
-          return;
-      }
-      xps_pipe_create(listener->core, DEFAULT_PIPE_BUFF_THRESH, file->source, client->sink);
+	xps_session_t * session = xps_session_create(listener->core,client);
+	if (session == NULL) {
+		logger(LOG_ERROR, "listener_connection_handler()", "xps_session_create() failed");
+		xps_connection_destroy(client);
+		return;
     }
-
-    else if (listener->port == 8003) {
-            int error;
-            xps_file_t *file = xps_file_create(listener->core, "../temp/file.txt", &error);
-            if (!file){
-              xps_connection_destroy(client);
-              return;
-            }
-            xps_pipe_create(listener->core, DEFAULT_PIPE_BUFF_THRESH, file->source, client->sink);
-        }
-
-    else{
-        xps_pipe_create(listener->core, DEFAULT_PIPE_BUFF_THRESH, client->source, client->sink);
-        client->listener = listener;
-    }
-    logger(LOG_INFO, "xps_listener_connection_handler()", "new connection");
-  }
+	logger(LOG_INFO, "xps_listener_connection_handler()", "new connection");
+	}
 }
